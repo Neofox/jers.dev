bea4995357fd89b3e89507633cb5ba6396ba32a9
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2023-06-30 20:24:23 +0900

    add gtag

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import Head from "../head";
 import Providers from "../Providers";
 import { dir } from "i18next";
+import { languages } from "../i18n/settings";
+import GoogleAnalytics from "@/components/Utils/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,6 @@ export const metadata = {
     title: "Jerome Schaeffer",
     description: "Resume of Jerome Schaeffer",
 };
-
-import { languages } from "../i18n/settings";
 
 export async function generateStaticParams() {
     return languages.map(lng => ({ lng }));
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang={lng} dir={dir(lng)} className="h-full scroll-smooth" suppressHydrationWarning>
             <Head />
-
+            <GoogleAnalytics GA_MEASUREMENT_ID="G-MLEGW0GG4H" />
             <body className={inter.className}>
                 <Providers>{children}</Providers>
             </body>

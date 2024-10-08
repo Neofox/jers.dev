c96297ac621929fed7344b5d25298ce5d96bc734
jerome <jerome@enhans.ai>
2024-10-08 15:17:02 +0900

    fix: type error

--- lib/storageHelper.ts
+++ lib/storageHelper.ts
@@ -1,11 +1,11 @@
 import "client-only";
 
-export function getLocalStorage(key: string, defaultValue: string) {
+export function getLocalStorage(key: string, defaultValue: unknown) {
     const stickyValue = localStorage.getItem(key);
 
     return stickyValue !== null && stickyValue !== "undefined" ? JSON.parse(stickyValue) : defaultValue;
 }
 
-export function setLocalStorage(key: string, value: string) {
+export function setLocalStorage(key: string, value: unknown) {
     localStorage.setItem(key, JSON.stringify(value));
 }
